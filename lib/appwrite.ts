5e5bd3366dcdc2865c58ae7a34110cc3ec8188df
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2022-09-11 18:10:55 +0200

    add seed api test

---
+++ lib/appwrite.ts
@@ -0,0 +1,13 @@
+import Appwrite from 'node-appwrite'
+
+export const initAppwrite = () => {
+  const sdk = new Appwrite.Client()
+  sdk
+    .setEndpoint(process.env.APPWRITE_ENDPOINT as string) // Your API Endpoint
+    .setProject(process.env.APPWRITE_PROJECT_ID as string) // Your project ID;
+    .setKey(process.env.APPWRITE_SERVER_API_KEY as string)
+  if(process.env.PUBLISH_ENV as string === "development"){
+    sdk.setSelfSigned()
+  }
+  return sdk
+}
